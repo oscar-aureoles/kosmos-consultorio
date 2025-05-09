1c08dd15d30aef4c832ce9cbba4ef9e27deccb03
Oscar Aureoles <oscar_jba@hotmail.com>
2025-05-09 14:10:41 -0600

    init repo

---
+++ src/test/java/kosmos/consultorio/ConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package kosmos.consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
